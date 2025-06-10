932e7a92aa56084fbd6c01e33da7f20d5ab3e887
Eliane <elianewilson2024@gmail.com>
2025-06-10 16:41:59 -0300

    Se agrego un condicional tambien para la actividad

--- Js/main.js
+++ Js/main.js
@@ -1,34 +1,37 @@
 //Primer paso: Calcular el Indice Metabolico Basal del usuario: 
-function CalculoTasaMetabolicaB (sexo, altura,peso, edad)  
-{
+function CalculoTasaMetabolicaB ( sexo, altura, peso, edad)  {
     let tmetbasal= 0
     if (sexo=="mujer"){
     tmetbasal = 447.6 + (9.2 * peso) + (3.1 * altura) - (4.3 * edad)
     } else if (sexo=="hombre"){
     tmetbasal = 88.36 + (13.4 * peso) + (4.8 * altura) - (5.7 * edad)
-  } else {
+    } else {
     console.log("Seleccionaste una opcion incorrecta, Usa 'hombre' o 'mujer'.")
     return null
-  }
+    }
   return tmetbasal
+}
 
-
+  //Restriccion por sexo. 
+  let sexo = "";
+  while (sexo !== "hombre" && sexo !== "mujer") {
+    sexo = prompt("A continuación ingrese su sexo: Mujer / Hombre").toLowerCase();
+    if (sexo !== "hombre" && sexo !== "mujer") {
+    alert("Opción inválida. Por favor ingrese 'hombre' o 'mujer'.");
+    }
   }
 
   //Ingreso de datos del usuario: 
 
-let sexo = prompt ("A continuación ingrese su sexo: Mujer / Hombre").toLowerCase()
-let peso = prompt ("A continuación ingrese su peso en kilogramos, ej: 73.400 kg, 60 kg")
-let altura = prompt ("Ingrese su altura en centimetros, ej: 1 metro 72 cm sería  172 cm ")
-let edad = prompt ("Ingrese su edad")
+
+let peso =Number (prompt ("A continuación ingrese su peso en kilogramos, ej: 73.400 kg, 60 kg"))
+let altura =Number (prompt ("Ingrese su altura en centimetros, ej: 1 metro 72 cm sería  172 cm "))
+let edad =Number (prompt ("Ingrese su edad"))
 
 
 let ResultadoTMB = CalculoTasaMetabolicaB (sexo, altura,peso, edad)
 
-if (ResultadoTMB !== null) {
-  alert("Tu Tasa Metabólica Basal es: " + ResultadoTMB + "Calorias por dia")
-  console.log("Tu Tasa Metabolica Basal es: " + ResultadoTMB + " calorías por día")
-}
+
 //ver alert , ver el prompt como variable cuantitativa - no string 
 
 //Para poder calcular las calorias segun objetivo, la formula requiere definir actividad
@@ -53,8 +56,17 @@ function CalculoActividad (tmb, nivelactividad){
       break
     default: 
       alert ("Opcion incorrecta")
-      
+      return null
   }
   return factoractividad*tmb
 
+}
+if (ResultadoTMB !== null) {
+  let actividad = Number (prompt ("Ingresa tu nivel de actividad física:  \n 1-Sedentario \n 2- Ligero \n3-Moderado (3 a 5 veces por semana) \n 4-Intenso (6 a 7 veces por semana) \n 5- Muy Intenso (2 veces por dia"))
+  if (actividad >= 1 && actividad <= 5){
+  let caloriasMantenimiento = CalculoActividad (ResultadoTMB, actividad)
+  alert("Tu Tasa Metabólica Basal es: " + ResultadoTMB + "  Calorias por dia")
+  console.log("Tu Tasa Metabolica Basal es: " + ResultadoTMB + " calorías por día")
+  console.log("Calorias para mantenimiento es: "+ caloriasMantenimiento + " por dia, manteniendo tu actividad")
+  }
 }
