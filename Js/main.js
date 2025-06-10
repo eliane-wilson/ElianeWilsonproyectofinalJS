cdc6f5db0187025b766f6c024d2ec4b82457a57b
Eliane <elianewilson2024@gmail.com>
2025-06-10 16:37:26 -0300

    Se aclararon las variables numericas, a pesar de que solo hay producto. Se corrigio number por Number

--- Js/main.js
+++ Js/main.js
@@ -10,6 +10,9 @@ function CalculoTasaMetabolicaB (sexo, altura,peso, edad)
     console.log("Seleccionaste una opcion incorrecta, Usa 'hombre' o 'mujer'.")
     return null
   }
+  return tmetbasal
+
+
   }
 
   //Ingreso de datos del usuario: 
@@ -23,6 +26,35 @@ let edad = prompt ("Ingrese su edad")
 let ResultadoTMB = CalculoTasaMetabolicaB (sexo, altura,peso, edad)
 
 if (ResultadoTMB !== null) {
-console.log("Tu TMB es: " + ResultadoTMB + " calorías por día")
+  alert("Tu Tasa Metabólica Basal es: " + ResultadoTMB + "Calorias por dia")
+  console.log("Tu Tasa Metabolica Basal es: " + ResultadoTMB + " calorías por día")
 }
 //ver alert , ver el prompt como variable cuantitativa - no string 
+
+//Para poder calcular las calorias segun objetivo, la formula requiere definir actividad
+
+function CalculoActividad (tmb, nivelactividad){
+  let factoractividad
+  switch (nivelactividad){
+    case 1: 
+      factoractividad=1.2
+      break
+    case 2:
+      factoractividad=1.375
+      break
+    case 3:
+      factoractividad=1.55
+      break
+    case 4:
+      factoractividad=1.725
+      break
+    case 5:
+      factoractividad=1.9
+      break
+    default: 
+      alert ("Opcion incorrecta")
+      
+  }
+  return factoractividad*tmb
+
+}
