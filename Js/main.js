3b0b513f3f2c1aa763bc20f3c17f7b31c12884b5
Eliane <elianewilson2024@gmail.com>
2025-06-26 11:10:03 -0300

    Se corrigieron las variables en mayuscula

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,5 @@
 //Primer paso: Calcular el Indice Metabolico Basal del usuario: 
-function CalculoTasaMetabolicaB ( sexo, altura, peso, edad)  {
+function calculotasametabolicaB ( sexo, altura, peso, edad)  {
     let tmetbasal= 0
     if (sexo=="mujer"){
     tmetbasal = 447.6 + (9.2 * peso) + (3.1 * altura) - (4.3 * edad)
@@ -29,14 +29,14 @@ let altura =Number (prompt ("Ingrese su altura en centimetros, ej: 1 metro 72 cm
 let edad =Number (prompt ("Ingrese su edad"))
 
 
-let ResultadoTMB = CalculoTasaMetabolicaB (sexo, altura,peso, edad)
+let resultadotmb = calculotasametabolicab (sexo, altura,peso, edad)
 
 
 //ver alert , ver el prompt como variable cuantitativa - no string 
 
 //Para poder calcular las calorias segun objetivo, la formula requiere definir actividad
 
-function CalculoActividad (tmb, nivelactividad){
+function calculoactividad (tmb, nivelactividad){
   let factoractividad
   switch (nivelactividad){
     case 1: 
@@ -61,22 +61,22 @@ function CalculoActividad (tmb, nivelactividad){
   return factoractividad*tmb
 
 }
-if (ResultadoTMB !== null) {
+if (resultadotmb !== null) {
   let actividad = Number (prompt ("Ingresa tu nivel de actividad física:  \n 1-Sedentario \n 2- Ligero \n3-Moderado (3 a 5 veces por semana) \n 4-Intenso (6 a 7 veces por semana) \n 5- Muy Intenso (2 veces por dia"))
   if (actividad >= 1 && actividad <= 5){
-  let caloriasMantenimiento = CalculoActividad (ResultadoTMB, actividad)
-  alert("Tu Tasa Metabólica Basal es: " + ResultadoTMB + "  Calorias por dia")
-  console.log("Tu Tasa Metabolica Basal es: " + ResultadoTMB + " calorías por día")
-  console.log("Calorias para mantenimiento es: "+ caloriasMantenimiento + " por dia, manteniendo tu actividad")
+  let caloriasMantenimiento = CalculoActividad (resultadotmb, actividad)
+  alert("Tu Tasa Metabólica Basal es: " + resultadotmb + "  Calorias por dia")
+  console.log("Tu Tasa Metabolica Basal es: " + resultadotmb + " calorías por día")
+  console.log("Calorias para mantenimiento es: "+ caloriasmantenimiento + " por dia, manteniendo tu actividad")
   }
 }
 //Array para objetivos de usuario //
 const objetivos = ["bajar de peso", "mantener peso", "ganar masa muscular", "tonificar"]
-let EstablecerObjetivo = Number (prompt ("A continuacion ingrese un objetivo :1- Bajar de peso, 2- Mantener, 3-Ganar Masa Muscular, 4-Tonificar "))
-if (EstablecerObjetivo < 1 || EstablecerObjetivo > 4 ) {
+let establecerobjetivo = Number (prompt ("A continuacion ingrese un objetivo :1- Bajar de peso, 2- Mantener, 3-Ganar Masa Muscular, 4-Tonificar "))
+if (establecerobjetivo < 1 || establecerbbjetivo > 4 ) {
   alert("Opción inválida. Recargá la página e intentá de nuevo.");
-} else { let objetivoelegido= objetivos[opcionObjetivo-1]}
+} else { let objetivoelegido= objetivos[opcionobjetivo-1]}
 
 //Calculo cuantas calorias tengo que gastar segun mi objetivo//
-let CaloriasObjetivo
-if (objetivoelegido==="Bajar de peso") {CaloriaObjetivo=CaloriasMantenimiento -500}
+let caloriasobjetivo
+if (objetivoelegido==="Bajar de peso") {caloriaobjetivo=caloriasmantenimiento -500}
